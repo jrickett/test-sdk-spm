c751271719da1366005afb38a9121953db7651f3
James Rickett <james.rickett@idverse.com>
2025-09-15 14:52:56 +1000

    Update Package.swift for 0.1.2

--- Package.swift
+++ Package.swift
@@ -2,19 +2,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "TestSDK",
+    name: "test-sdk-spm",
     defaultLocalization: "en",
-    platforms: [.iOS(.v15)],
-    products: [
-        .library(name: "TestFaceScanUI", targets: ["TestFaceScanUI"]),
-        .library(name: "TestIDScanUI",   targets: ["TestIDScanUI"]),
-        .library(name: "TestUI",         targets: ["TestFaceScanUI", "TestIDScanUI"])
+    platforms: [
+        .iOS(.v15)
     ],
-    dependencies: [],
+    products: [
+        // UI products include TestCommon so the app links it automatically.
+        .library(
+            name: "TestIDScanUI",
+            targets: ["TestCommon", "TestIDScanUI"]
+        ),
+        .library(
+            name: "TestFaceScanUI",
+            targets: ["TestCommon", "TestFaceScanUI"]
+        ),
+    ],
     targets: [
-        // We'll replace these with `url:` + `checksum:` once the Release is created.
-        // For now, leave placeholders so the repo can build.
-        .binaryTarget(name: "TestFaceScanUI", url: "https://github.com/jrickett/test-sdk-spm/releases/download/0.1.1/TestFaceScanUI-0.1.1.zip", checksum: "4c064b8706959bda5e108d48859858cef77bc9080ca23dbab2cecabb6b96842f"),
-        .binaryTarget(name: "TestIDScanUI", url: "https://github.com/jrickett/test-sdk-spm/releases/download/0.1.1/TestIDScanUI-0.1.1.zip", checksum: "a7d769267ddab5d6ff8f68ad7728fb35cf425c7c8a6c8a168e3f378fcd2320cc")
+        // NOTE: The URLs & checksums are filled in by your publish script.
+        .binaryTarget(name: "TestCommon", url: "https://github.com/jrickett/test-sdk-spm/releases/download/0.1.2/TestCommon-0.1.2.zip", checksum: "590663d2335f8838102dc39f07316c79d9943722517d7315c11f31173c9d5627"),
+        .binaryTarget(name: "TestIDScanUI", url: "https://github.com/jrickett/test-sdk-spm/releases/download/0.1.2/TestIDScanUI-0.1.2.zip", checksum: "c4bd47f73d4bca871bdc0baed15481a6e66debaab321003df387972272c85fbc"),
+        .binaryTarget(name: "TestFaceScanUI", url: "https://github.com/jrickett/test-sdk-spm/releases/download/0.1.2/TestFaceScanUI-0.1.2.zip", checksum: "0e9573562523eb5b1bd126758fa2ee85ffd99a748db80bc17dc74ccea291221a"),
     ]
 )
